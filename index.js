66d4e2c68d7ef2dfb0c0fb661d16e7b651cdb919
lord2anil <a8208226@gmail.com>
2023-07-26 12:32:56 +0530

    upadte

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://master--chatapp11312.netlify.app/login",
     credentials: true,
   },
 });
